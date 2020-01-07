226e6eab365ffdd3b70935adfae959698d90f9bc
Hecklebot <iuehakdrk@gmail.com>
2020-01-07 09:58:47 +0900

    TodoItemList function component 변경

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-class TodoItemList extends Component {
-  render() {
-    const { todos, onToggle, onRemove } = this.props;
-    const todoList = todos.map(({ id, text, checked }) => <TodoItem id={id} text={text} checked={checked} onToggle={onToggle} onRemove={onRemove} key={id} />);
-    return <div>{todoList}</div>;
-  }
-}
+const TodoItemList = ({ todos, onToggle, onRemove }) => {
+  const todoList = todos.map(({ id, text, checked }) => <TodoItem id={id} text={text} checked={checked} onToggle={onToggle} onRemove={onRemove} key={id} />);
+  return <div>{todoList}</div>;
+};
 
 TodoItemList.propTypes = {
   todos: PropTypes.array,
